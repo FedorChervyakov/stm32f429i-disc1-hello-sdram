1b30d3172e6e8b853b0d6a65e51f4f9917550e33
Fedor Chervyakov <fchervyakov@gmail.com>
2022-03-20 02:04:19 +0300

    Initial commit

---
+++ examples/hello.rs
@@ -0,0 +1,18 @@
+//! Prints "Hello, world!" on the host console using semihosting
+
+#![no_main]
+#![no_std]
+
+use panic_halt as _;
+
+use cortex_m_rt::entry;
+use rtt_target::{rtt_init_print, rprintln};
+
+#[entry]
+fn main() -> ! {
+    rtt_init_print!();
+
+    rprintln!("Hellow!!!");
+
+    loop {}
+}
